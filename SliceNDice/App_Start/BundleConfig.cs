1ee25083dafbe35370c880a4f697b1395fffab04
Luke Forder <luke.forder@gmail.com>
2013-09-30 14:33:07 -0700

    source code commit

---
+++ SliceNDice/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SliceNDice
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                "~/Scripts/knockout-{version}.debug.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/linq").Include(
+               "~/Scripts/linq.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
+               "~/Scripts/toastr.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootswatch.cosmo.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/toastr").Include(
+               "~/Content/toastr.css"));
+        }
+    }
+}
---
+++ SliceNDice/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-2.0.3.js" />
+/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="knockout-2.3.0.js" />
+/// <reference path="linq.js" />
+/// <reference path="toastr.js" />
